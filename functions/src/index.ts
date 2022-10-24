ce6fc6498162cc3c28c87c7aafd4d229a13458e7
Vasylenko Paul <maincrafter1307@gmail.com>
2022-10-25 00:02:51 +0300

    Fix: change deploy fn

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,11 @@
 import * as functions from 'firebase-functions';
+import { Request, Response } from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 
-export const helloWorld = functions.https.onRequest((req, res) => {
+export const helloWorld = functions.https.onRequest((req: Request, res: Response) => {
 	functions.logger.log('Testing firebase logger here');
 
 	res.json({
